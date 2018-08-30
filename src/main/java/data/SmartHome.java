f0ef8dd1afa33893c922e0ec6f399009db7aaf51
Marcin <Majsyn@gmail.com>
2018-08-30 12:46:14 +0200

    V 0.2

--- src/main/java/data/SmartHome.java
+++ src/main/java/data/SmartHome.java
@@ -5,14 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class SmartHome {
 
-    SpeakerController speakerController;
-    RollerBlindController rollerBlindController;
-    StoveController stoveController;
 
-    public SmartHome(SpeakerController speakerController, RollerBlindController rollerBlindController, StoveController stoveController) {
-        this.speakerController = speakerController;
-        this.rollerBlindController = rollerBlindController;
-        this.stoveController = stoveController;
-
-    }
 }
